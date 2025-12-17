7542a2d92ec346c4c5076f4300084dcfb88ec2c9
ganguo <ganguo>
2025-12-17 17:01:39 +0800

    added 3rd libs using brew

---
+++ build/libclix.build/Debug/libclix.build/Objects-normal/arm64/clix-ocr.d
@@ -0,0 +1,50 @@
+dependencies: \
+  /Users/christian/export/local/works/doublegsoft.me/myhotkey/03.Development/libclix/src/clix-ocr.cpp \
+  /Users/christian/export/local/works/doublegsoft.me/myhotkey/03.Development/libclix/src/clix-ocr.hpp \
+  3rd/gfc/include/gfc.h 3rd/gfc/include/gfc_type.h \
+  3rd/gfc/include/gfc_string.h 3rd/gfc/include/gfc_list.h \
+  3rd/gfc/include/gfc_utf8.h 3rd/gfc/include/gfc_date.h \
+  3rd/gfc/include/gfc_uuid.h 3rd/gfc/include/gfc_map.h \
+  3rd/gfc/include/gfc_ring.h 3rd/gfc/include/gfc_lru.h \
+  3rd/gfc/include/gfc_log.h 3rd/gfc/include/gfc_sql.h \
+  3rd/gfc/include/gfc_crypto.h 3rd/gfc/include/gfc_tcp.h \
+  3rd/gfc/include/gfc_ws.h 3rd/gfc/include/gfc_mt.h \
+  3rd/gfc/include/gfc_gc.h 3rd/gfc/include/gfc_fs.h \
+  3rd/gfc/include/gfc_cli.h 3rd/gfc/include/gfc_jvm.h \
+  3rd/gfc/include/gfc_lua.h 3rd/gfc/include/gfc_id.h \
+  /opt/homebrew/Cellar/tesseract/5.5.1_1/include/tesseract/baseapi.h \
+  /opt/homebrew/Cellar/tesseract/5.5.1_1/include/tesseract/export.h \
+  /opt/homebrew/Cellar/tesseract/5.5.1_1/include/tesseract/pageiterator.h \
+  /opt/homebrew/Cellar/tesseract/5.5.1_1/include/tesseract/publictypes.h \
+  /opt/homebrew/Cellar/tesseract/5.5.1_1/include/tesseract/resultiterator.h \
+  /opt/homebrew/Cellar/tesseract/5.5.1_1/include/tesseract/ltrresultiterator.h \
+  /opt/homebrew/Cellar/tesseract/5.5.1_1/include/tesseract/unichar.h \
+  /opt/homebrew/Cellar/tesseract/5.5.1_1/include/tesseract/version.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/allheaders.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/alltypes.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/endianness.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/environ.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/array.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/bbuffer.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/hashmap.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/heap.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/list.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/ptra.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/queue.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/rbtree.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/stack.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/arrayaccess.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/bmf.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/ccbord.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/colorfill.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/dewarp.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/gplot.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/imageio.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/jbclass.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/morph.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/pix.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/recog.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/regutils.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/stringcode.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/sudoku.h \
+  /opt/homebrew/Cellar/leptonica/1.85.0/include/leptonica/watershed.h
