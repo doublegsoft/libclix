60a322fe7bdb120d384ee65b54d67d856eb994d1
ganguo <ganguo>
2026-02-23 18:59:15 +0800

    bmp to png

--- src/clix-win.c
+++ src/clix-win.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <png.h>
 
 #include "clix-desktop.h"
 
@@ -72,6 +73,54 @@ clix_click()
   inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
 
   SendInput(2, inputs, sizeof(INPUT));
+}
+
+static int 
+clix_save_as_png(const char* filename,
+                 unsigned char* rgb,
+                 int width,
+                 int height)
+{
+  FILE* fp = fopen(filename, "wb");
+  if (!fp) return -1;
+
+  png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING,
+                                            NULL, NULL, NULL);
+  if (!png) return -1;
+
+  png_infop info = png_create_info_struct(png);
+  if (!info) return -1;
+
+  if (setjmp(png_jmpbuf(png)))
+  return -1;
+
+  png_init_io(png, fp);
+
+  png_set_IHDR(png,
+               info,
+               width,
+               height,
+               8,
+               PNG_COLOR_TYPE_RGB,
+               PNG_INTERLACE_NONE,
+               PNG_COMPRESSION_TYPE_BASE,
+               PNG_FILTER_TYPE_BASE);
+
+  png_write_info(png, info);
+
+  png_bytep* rows = (png_bytep*)malloc(sizeof(png_bytep) * height);
+
+  for (int y = 0; y < height; y++)
+    rows[y] = rgb + y * width * 3;
+
+  png_write_image(png, rows);
+  png_write_end(png, NULL);
+
+  free(rows);
+  fclose(fp);
+  png_destroy_write_struct(&png, &info);
+
+  return 0;
 }
 
 void 
@@ -160,24 +209,26 @@ clix_screen_capture(clix_context_t* ctx, const char* path)
   GetDIBits(hDC, hBitmap, 0, height, bmpBuffer,
             (BITMAPINFO*)&bi, DIB_RGB_COLORS);
 
-  FILE* file = fopen(path, "wb");
+  clix_save_as_png(path, bmpBuffer, width, height);
 
-  bmfHeader.bfType = 0x4D42;
-  bmfHeader.bfSize =
-    sizeof(BITMAPFILEHEADER) +
-    sizeof(BITMAPINFOHEADER) +
-    dwBmpSize;
-  bmfHeader.bfReserved1 = 0;
-  bmfHeader.bfReserved2 = 0;
-  bmfHeader.bfOffBits =
-    sizeof(BITMAPFILEHEADER) +
-    sizeof(BITMAPINFOHEADER);
+  // FILE* file = fopen(path, "wb");
 
-  fwrite(&bmfHeader, sizeof(BITMAPFILEHEADER), 1, file);
-  fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, file);
-  fwrite(bmpBuffer, dwBmpSize, 1, file);
+  // bmfHeader.bfType = 0x4D42;
+  // bmfHeader.bfSize =
+  //   sizeof(BITMAPFILEHEADER) +
+  //   sizeof(BITMAPINFOHEADER) +
+  //   dwBmpSize;
+  // bmfHeader.bfReserved1 = 0;
+  // bmfHeader.bfReserved2 = 0;
+  // bmfHeader.bfOffBits =
+  //   sizeof(BITMAPFILEHEADER) +
+  //   sizeof(BITMAPINFOHEADER);
 
-  fclose(file);
+  // fwrite(&bmfHeader, sizeof(BITMAPFILEHEADER), 1, file);
+  // fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, file);
+  // fwrite(bmpBuffer, dwBmpSize, 1, file);
+
+  // fclose(file);
 
   free(bmpBuffer);
   DeleteObject(hBitmap);
